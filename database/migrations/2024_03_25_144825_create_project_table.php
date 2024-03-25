826fc9e02dc0fe3d1033c658d75150ced02af226
easycheaplife <lijin157@163.com>
2024-03-25 22:54:59 +0800

    Initial commit

---
+++ database/migrations/2024_03_25_144825_create_project_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->string('token', 64);
+            $table->string('email', 64);
+            $table->string('logo', 64);
+            $table->string('name', 64);
+            $table->string('desc', 64);
+            $table->string('website', 128);
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project');
+    }
+};
