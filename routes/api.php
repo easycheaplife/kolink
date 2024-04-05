e0fe45ad22cb06f79134628ea4fe603ce3cec103
easycheaplife <lijin157@163.com>
2024-04-05 23:48:04 +0800

    add log for all request/response

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('log.request.response')->group(function () {
+
+});
+
 
 Route::get('/test', [TestController::class, 'index']);
 
