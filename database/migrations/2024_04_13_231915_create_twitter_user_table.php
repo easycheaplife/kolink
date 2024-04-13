e7589d72bc1f854c3981e6cc5c28fca82159859a
easycheaplife <lijin157@163.com>
2024-04-14 07:41:17 +0800

    create tabel twitter user

---
+++ database/migrations/2024_04_13_231915_create_twitter_user_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('twitter_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->default(0);
+            $table->string('name', 128)->default('');
+            $table->string('screen_name', 128)->default('');
+            $table->string('location', 256)->default('');
+            $table->string('description', 512)->default('');
+            $table->string('url', 512)->default('');
+            $table->integer('followers_count')->default(0);
+            $table->integer('friends_count')->default(0);
+            $table->integer('listed_count')->default(0);
+            $table->integer('favourites_count')->default(0);
+            $table->integer('utc_offset')->default(0);
+            $table->string('time_zone', 128)->default('');
+            $table->integer('geo_enabled')->default(0);
+            $table->integer('verified')->default(0);
+            $table->integer('statuses_count')->default(0);
+            $table->string('lang', 128)->default('');
+            $table->string('profile_background_image_url', 256)->default('');
+            $table->string('profile_background_image_url_https', 256)->default('');
+            $table->string('profile_image_url', 256)->default('');
+            $table->string('profile_image_url_https', 256)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('twitter_user');
+    }
+};
