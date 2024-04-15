3771c5ddab6c990190d39c264a0dde6db6aef997
sky clean <skyclean@skys-MacBook-Pro.local>
2024-04-15 22:33:15 +0800

    add session for api

--- script/twitter/user_auth.py
+++ script/twitter/user_auth.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 consumer_key = 'OUdXhQ3o52StkWakpcOCLew8s'
 consumer_secret = 'jqdlE388wxf8YutH5O06Fjsa157sJTy0R9h8SNVepf65tYrxWl'
 callback_url = 'https://gold-breads-occur.loca.lt/auth/twitter/callback'
+session = {}
 
 host = "0.0.0.0"
 port = 8000
@@ -29,21 +30,23 @@ logging.basicConfig(filename=log_file, level=logging.DEBUG)
 def index():
     return '<a href="/auth/twitter">Login with Twitter</a>'
 
-@app.route('/auth/twitter')
+@app.route('/api/auth/twitter')
 def auth_twitter():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
     auth_url = auth.get_authorization_url()
-    return redirect(auth_url)
+    session['request_token'] = auth.request_token
+    response = {
+        "code": 0,
+        "message": "",
+        "data": {"url": auth_url}
+    }
+    return response
 
-@app.route('/auth/twitter/callback')
+@app.route('/api/user/twitter')
 def auth_twitter_callback():
-    token = request.args.get('oauth_token')
     verifier = request.args.get('oauth_verifier')
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
-    auth.request_token = {
-        'oauth_token': token,
-        'oauth_token_secret': verifier
-    }
+    auth.request_token = session['request_token']
     auth.get_access_token(verifier)
     access_token = auth.access_token
     access_token_secret = auth.access_token_secret
