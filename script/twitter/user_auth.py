d8bd6c6f66268289778b0743e611bc69a6389b93
Cloud User <ec2-user@ip-172-31-52-158.ec2.internal>
2024-04-20 03:56:47 +0000

    update key

--- script/twitter/user_auth.py
+++ script/twitter/user_auth.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-consumer_key = 'gfJSbufYP2dDLGx9A4nYrUBQf'
-consumer_secret = 'W7qE8naK1D3v6zXn6njkkL30M7EQAk39klGkZmFbY4Wl8yRNTh'
+consumer_key = 'viwaiJCd5kfpdsPPrSTjp2cdu'
+consumer_secret = '71V1vBfn4ua110MZajNG2Z6MabUM5UoS9NJE1fo5BNQMKrv3gD'
 callback_url = 'http://localhost:8888/oauth'
 session = {}
 
@@ -94,7 +94,6 @@ def auth_twitter():
     response['data']['session_id'] = session_id
     if session_id not in session:
         session[session_id] = {}
-    return auth_res
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback = redirect_uri)
     auth_url = auth.get_authorization_url()
     session[session_id]['request_token'] = auth.request_token
@@ -111,7 +110,6 @@ def auth_twitter_callback():
     session_id = request.args.get('session_id')
     verifier = request.args.get('oauth_verifier')
     redirect_uri = request.args.get('redirect_uri', callback_url)
-    return user_res
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback = redirect_uri)
     auth.request_token = session[session_id]['request_token']
     auth.get_access_token(verifier)
