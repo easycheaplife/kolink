26bc355af98f4a04b25b11d92c8a453c2489bc8d
Cloud User <ec2-user@ip-172-31-52-158.ec2.internal>
2024-08-21 08:13:36 +0000

    add content relevance

---
+++ app/Models/TwitterContentRelevanceModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TwitterContentRelevanceModel extends Model
+{
+    use HasFactory;
+	protected $table = 'twitter_content_relevance';
+	protected $fillable = [
+		'user_id', 'user_name', 'category_id', 'score', 'explanation' 
+	];
+
+	public function insert($user_id, $user_name, $category_id, $score, $explanation)
+	{
+		try {
+			return $this->updateOrCreate(
+				['user_id' => $user_name, 'category_id' => $category_id],
+				[
+					'user_id' => $user_id,
+					'user_name' => $user_name,
+					'category_id' => $category_id,
+					'score' => $score,
+					'explanation' => $explanation,
+				]);
+		}
+		catch (QueryException $e)
+		{
+			Log::error($e->getMessage());
+		}
+		return false;
+	}
+
+	public function get($user_id, $category_id)
+	{
+		return $this->select('user_id', 'user_name', 'category_id', 'score', 'explanation') 
+			->where('user_id', $scree_id)
+			->where('category_id', $category_id)
+			->first();
+	}
+
+}
